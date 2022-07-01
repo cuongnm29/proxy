7f14a3b1333383c65e3554d7027b0867df1951ea
nguyencuong <nguyencuong.fpt2013@gmail.com>
2022-07-01 17:15:10 +0700

    coupon

---
+++ resources/views/admin/coupon/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.create') }} {{ trans('cruds.coupon.title_singular') }}
+    </div>
+
+    <div class="card-body">
+        <form action="{{ route('admin.coupon.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group {{ $errors->has('percent') ? 'has-error' : '' }}">
+                <label for="name">{{ trans('cruds.coupon.fields.percent') }}*</label>
+                <input type="number" id="percent" name="percent" class="form-control" value="{{ old('percent', isset($coupon) ? $coupon->percent : '') }}" required>
+                @if($errors->has('percent'))
+                    <em class="invalid-feedback">
+                        {{ $errors->first('percent') }}
+                    </em>
+                @endif
+                <p class="helper-block">
+                    {{ trans('cruds.coupon.fields.percent_helper') }}
+                </p>
+            </div>
+            <div class="form-group {{ $errors->has('status') ? 'has-error' : '' }}">
+                <label for="status">{{ trans('cruds.coupon.fields.status') }}
+                </label>
+                <select name="status" id="status" class="form-control select">
+                    <option value="1">Enable</option>
+                    <option value="0">Disable</option>
+                </select>
+                @if($errors->has('status'))
+                <em class="invalid-feedback">
+                    {{ $errors->first('status') }}
+                </em>
+                @endif
+                <p class="helper-block">
+                    {{ trans('cruds.coupon.fields.status_helper') }}
+                </p>
+            </div>
+            
+            <div>
+                <input class="btn btn-danger" type="submit" value="{{ trans('global.save') }}">
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+
---
+++ resources/views/admin/coupon/index.blade.php
@@ -0,0 +1,131 @@
+@extends('layouts.admin')
+@section('content')
+@can('coupon_manage')
+    <div style="margin-bottom: 10px;" class="row">
+        <div class="col-lg-12">
+            <a class="btn btn-success" href="{{ route("admin.coupon.create") }}">
+                {{ trans('global.add') }} {{ trans('cruds.coupon.title_singular') }}
+            </a>
+        </div>
+    </div>
+@endcan
+<div class="card">
+    <div class="card-header">
+        {{ trans('cruds.coupon.title_singular') }} {{ trans('global.list') }}
+    </div>
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class=" table table-bordered table-striped table-hover datatable datatable-coupon">
+                <thead>
+                    <tr>
+                        <th width="10">
+
+                        </th>
+                        <th>
+                            {{ trans('cruds.coupon.fields.id') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.coupon.fields.name') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.coupon.fields.percent') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.coupon.fields.status') }}
+                        </th>
+                        <th>
+                            &nbsp;
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($countries as $key => $coupon)
+                        <tr data-entry-id="{{ $coupon->id }}">
+                            <td>
+
+                            </td>
+                            <td>
+                                {{ $coupon->id ?? '' }}
+                            </td>
+                            <td>
+                                {{ $coupon->name ?? '' }}
+                            </td>
+                            <td>
+                                {{ $coupon->percent ?? '' }}
+                            </td>
+                            <td>
+                                {{ $coupon->status == "1" ? 'Enable':'Disable' }}
+                            </td>
+                            <td>
+                               
+                                <a class="btn btn-xs btn-info" href="{{ route('admin.coupon.edit', $coupon->id) }}">
+                                    {{ trans('global.edit') }}
+                                </a>
+
+                                <form action="{{ route('admin.coupon.destroy', $coupon->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                </form>
+
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+</div>
+@endsection
+@section('scripts')
+@parent
+<script>
+    $(function () {
+  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+@can('coupon_manage')
+  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
+  let deleteButton = {
+    text: deleteButtonTrans,
+    url: "{{ route('admin.coupon.mass_destroy') }}",
+    className: 'btn-danger',
+    action: function (e, dt, node, config) {
+      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+          return $(entry).data('entry-id')
+      });
+
+      if (ids.length === 0) {
+        alert('{{ trans('global.datatables.zero_selected') }}')
+
+        return
+      }
+
+      if (confirm('{{ trans('global.areYouSure') }}')) {
+        $.ajax({
+          headers: {'x-csrf-token': _token},
+          method: 'POST',
+          url: config.url,
+          data: { ids: ids, _method: 'DELETE' }})
+          .done(function () { location.reload() })
+      }
+    }
+  }
+  dtButtons.push(deleteButton)
+@endcan
+
+  $.extend(true, $.fn.dataTable.defaults, {
+    order: [[ 1, 'desc' ]],
+    pageLength: 100,
+  });
+  $('.datatable-coupon:not(.ajaxTable)').DataTable({ buttons: dtButtons })
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+        $($.fn.dataTable.tables(true)).DataTable()
+            .columns.adjust();
+    });
+})
+
+</script>
+@endsection
